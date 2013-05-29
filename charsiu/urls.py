20f6eb564901ca0fa3aaf1ecbae5ae378e8c129a
Andrew Pendleton <apendleton@sunlightfoundation.com>
2013-05-29 17:30:25 -0400

    Add auth.

--- charsiu/urls.py
+++ charsiu/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import patterns, include, url
 from charsiu.views import *
 
+from django.contrib.auth.decorators import login_required
+
 urlpatterns = patterns('',
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^next$', NextView.as_view(), name='next'),
-    url(r'^skip/(?P<document_id>[A-Z0-9_-]+$)', SkipView.as_view(), name='skip'),
-    url(r'^comment/(?P<document_id>[A-Z0-9_-]+$)', CommentView.as_view(), name='comment'),
+    url(r'^$', login_required(IndexView.as_view()), name='index'),
+    url(r'^next$', login_required(NextView.as_view()), name='next'),
+    url(r'^skip/(?P<document_id>[A-Z0-9_-]+$)', login_required(SkipView.as_view()), name='skip'),
+    url(r'^comment/(?P<document_id>[A-Z0-9_-]+$)', login_required(CommentView.as_view()), name='comment'),
+
+    # auth
+    (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
 )
